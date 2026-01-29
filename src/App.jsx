6a974bbff2264a43e7ebb317ee902726bdcfe0ee
Hadel Aljasem <hadelaljasem@uni-muenster.de>
2026-01-29 21:28:47 +0100

    app.jsx-editing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Header />
       <main className="container">
-        <Routes></Routes>
+        <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/contact" element={<Contact />} />
+           <Route path="/login" element={<Login />} />
+        </Routes>
       </main>
     </BrowserRouter>
+
+    <Footer/>
+    </>
   );
 }
 
