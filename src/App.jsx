9ed0a6874c6b3eed19822ac79986268dd7af152f
walid-dev16 <walidrafiq71@gmail.com>
2026-01-31 03:07:56 +0100

    Add Contact route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <main className="container">
-        <Routes></Routes>
+        <Routes> 
+       <Route path="/Contact"
+       element={<Contact/>}/>   
+        </Routes>
       </main>
     </BrowserRouter>
   );
